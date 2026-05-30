06660efeac8c9852fa26138ffcf8907d5c036c03
nuJack <57935141+nuJack@users.noreply.github.com>
2026-05-29 22:44:09 -0500

    Create teams.ts

---
+++ sim/examples/teams.ts
@@ -0,0 +1,91 @@
+// pokemon-showdown/sim/examples/teams.ts
+
+export const TEAM_A = [
+  {
+    name: "Mewtwo",
+    species: "Mewtwo",
+    moves: ["psychic", "recover", "amnesia", "thunderbolt"],
+    evs: {hp: 252, atk: 252, def: 252, spa: 252, spd: 252, spe: 252},
+    ivs: {hp: 30, atk: 30, def: 30, spa: 30, spd: 30, spe: 30},
+  },
+  {
+    name: "Mew",
+    species: "Mew",
+    moves: ["swordsdance", "bodyslam", "earthquake", "softboiled"],
+    evs: {hp: 252, atk: 252, def: 252, spa: 252, spd: 252, spe: 252},
+    ivs: {hp: 30, atk: 30, def: 30, spa: 30, spd: 30, spe: 30},
+  },
+  {
+    name: "Tauros",
+    species: "Tauros",
+    moves: ["bodyslam", "hyperbeam", "earthquake", "blizzard"],
+    evs: {hp: 252, atk: 252, def: 252, spa: 252, spd: 252, spe: 252},
+    ivs: {hp: 30, atk: 30, def: 30, spa: 30, spd: 30, spe: 30},
+  },
+  {
+    name: "Chansey",
+    species: "Chansey",
+    moves: ["icebeam", "thunderbolt", "softboiled", "thunderwave"],
+    evs: {hp: 252, atk: 252, def: 252, spa: 252, spd: 252, spe: 252},
+    ivs: {hp: 30, atk: 30, def: 30, spa: 30, spd: 30, spe: 30},
+  },
+  {
+    name: "Snorlax",
+    species: "Snorlax",
+    moves: ["bodyslam", "hyperbeam", "earthquake", "rest"],
+    evs: {hp: 252, atk: 252, def: 252, spa: 252, spd: 252, spe: 252},
+    ivs: {hp: 30, atk: 30, def: 30, spa: 30, spd: 30, spe: 30},
+  },
+  {
+    name: "Exeggutor",
+    species: "Exeggutor",
+    moves: ["sleeppowder", "psychic", "explosion", "stunspore"],
+    evs: {hp: 252, atk: 252, def: 252, spa: 252, spd: 252, spe: 252},
+    ivs: {hp: 30, atk: 30, def: 30, spa: 30, spd: 30, spe: 30},
+  },
+];
+
+export const TEAM_B = [
+  {
+    name: "Mewtwo",
+    species: "Mewtwo",
+    moves: ["psychic", "recover", "amnesia", "icebeam"],
+    evs: {hp: 252, atk: 252, def: 252, spa: 252, spd: 252, spe: 252},
+    ivs: {hp: 30, atk: 30, def: 30, spa: 30, spd: 30, spe: 30},
+  },
+  {
+    name: "Mew",
+    species: "Mew",
+    moves: ["thunderwave", "psychic", "softboiled", "explosion"],
+    evs: {hp: 252, atk: 252, def: 252, spa: 252, spd: 252, spe: 252},
+    ivs: {hp: 30, atk: 30, def: 30, spa: 30, spd: 30, spe: 30},
+  },
+  {
+    name: "Tauros",
+    species: "Tauros",
+    moves: ["bodyslam", "hyperbeam", "earthquake", "thunderbolt"],
+    evs: {hp: 252, atk: 252, def: 252, spa: 252, spd: 252, spe: 252},
+    ivs: {hp: 30, atk: 30, def: 30, spa: 30, spd: 30, spe: 30},
+  },
+  {
+    name: "Chansey",
+    species: "Chansey",
+    moves: ["seismictoss", "softboiled", "thunderwave", "reflect"],
+    evs: {hp: 252, atk: 252, def: 252, spa: 252, spd: 252, spe: 252},
+    ivs: {hp: 30, atk: 30, def: 30, spa: 30, spd: 30, spe: 30},
+  },
+  {
+    name: "Slowbro",
+    species: "Slowbro",
+    moves: ["amnesia", "surf", "rest", "thunderwave"],
+    evs: {hp: 252, atk: 252, def: 252, spa: 252, spd: 252, spe: 252},
+    ivs: {hp: 30, atk: 30, def: 30, spa: 30, spd: 30, spe: 30},
+  },
+  {
+    name: "Gengar",
+    species: "Gengar",
+    moves: ["hypnosis", "thunderbolt", "nightshade", "explosion"],
+    evs: {hp: 252, atk: 252, def: 252, spa: 252, spd: 252, spe: 252},
+    ivs: {hp: 30, atk: 30, def: 30, spa: 30, spd: 30, spe: 30},
+  },
+];
